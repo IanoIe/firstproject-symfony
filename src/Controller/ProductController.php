36fa211db282d46bf4e10717743ac3462e000de4
AC_Marvel <gil_1_10@hotmail.com>
2025-06-19 17:02:07 +0200

    edit, delete, boostrap..

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductForm;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ProductController extends AbstractController
 {
@@ -18,10 +21,56 @@ final class ProductController extends AbstractController
         ]);
     }
 
+    #[Route('/products/new', name: 'app_product_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductForm::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($product);
+            $em->flush();
+            return $this->redirectToRoute('app_products');
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
     #[Route('/products/{id<\d+>}', name: 'app_product')]
     public function show(Product $product): Response {
         return $this->render('product/show.html.twig', [
             'product' => $product,
         ]);
+    }
+
+    #[Route('/products/{id<\d+>}/edit', name: 'app_product_edit')]
+    public function edit(Product $product, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ProductForm::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($product);
+            $em->flush();
+            return $this->redirectToRoute('app_products');
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/products/{id<\d+>}/delete', name: 'app_product_delete')]
+    public function delete(Product $product, EntityManagerInterface $em): Response
+    {
+        $em->remove($product);
+        $em->flush();
+        return $this->redirectToRoute('app_products');
     }
 }
